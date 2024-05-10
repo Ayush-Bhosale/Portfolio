6efce8e106466d67df650a4f05d6624ed60eb826
Ayush-Bhosale <ayushbhosale7398@gmail.com>
2024-05-10 19:01:42 +0530

    changes

--- pages/flexagram.jsx
+++ pages/flexagram.jsx
@@ -31,8 +31,8 @@ const flexagram = () => {
             of Freelaning + Relevel Platform Freelancing Platform is currently hosted 
             but relevel is on localhost.I built this application in Next JS and is hosted on Vercel.
             This app features user authentication with OTP & Gmail Verification
-            platform allows users to create profiles, upload their work, 
-            manage transactions, and potentially participate in Relevel quiz app. 
+            platform allows users to create profiles, upload their work,
+            manage transactions, and potentially participate in Relevel quiz app.
             our aim was to attract big players to our platform just like instagram, x, facebook, etc. 
           </p>
           <a
